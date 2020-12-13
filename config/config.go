e9855f359d1098ec61dee91b0f02e411e5d7a521
hugoram <hugoram78@gmail.com>
2020-12-13 09:55:30 -0600

    Create spotify model and core service.

--- config/config.go
+++ config/config.go
@@ -3,5 +3,8 @@ package config
 import "os"
 
 var AppURL = os.Getenv("APP_URL")
+var SpotifyApiURL = "https://api.spotify.com/v1"
+var SpotifyAuth = os.Getenv("SPOTIFY_AUTH")
+var SpotifyAuthURL = "https://accounts.spotify.com/api/token"
 var TelegramToken = os.Getenv("TELEGRAM_TOKEN")
 var TelegramApiURL = "https://api.telegram.org/bot" + TelegramToken
